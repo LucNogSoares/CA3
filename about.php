a9e6ca3e9fec6fc3227dc3472168a74c4d2bbaf7
luigi-raynel-dev <devluigiraynel@gmail.com>
2024-05-17 18:00:01 -0300

    fix: move on avatar group to right

--- about.php
+++ about.php
@@ -3,46 +3,43 @@
 <div>
 <h1>About us</h1>
 
-<div>
-    <h2>Our History</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
-</div>
-<br>
-<div>
-    <h2>Make Home in Ireland for Generations</h2>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
-</div>
-<div>
-    <h2>Our Team</h2>
-    <div class="avatar-group">
-        <div class="avatar">
-            <img src="images/jhon_doe.jpg" alt="jon_doe">
-            <h4>Jhon Doe</h4>
+<div class="flex flex-column-lg w-full justify-content-between">
+    <div>
+        <div>
+            <h2>Our History</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
         </div>
-        <div class="avatar">
-            <img src="images/jane_doe.avif" alt="jane_doe">
-            <h4>Jane Doe</h4>
+        <br>
+        <div>
+            <h2>Make Home in Ireland for Generations</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
         </div>
-        <div class="avatar">
-            <img src="images/avatar.jpg" alt="avatar">
-            <h4>Lorem Ipsum</h4>
-        </div>
-        <div class="avatar">
-            <img src="images/jhon_doe.jpg" alt="jon_doe">
-            <h4>Jhon Doe</h4>
-        </div>
-        <div class="avatar">
-            <img src="images/jane_doe.avif" alt="jane_doe">
-            <h4>Jane Doe</h4>
-        </div>
-        <div class="avatar">
-            <img src="images/avatar.jpg" alt="avatar">
-            <h4>Lorem Ipsum</h4>
+    </div>
+    <div>
+        <h2>Our Team</h2>
+        <div class="avatar-group">
+            <div class="avatar w-half">
+                <img src="images/jhon_doe.jpg" alt="jon_doe">
+                <h4>Jhon Doe</h4>
+            </div>
+            <div class="avatar w-half">
+                <img src="images/jane_doe.avif" alt="jane_doe">
+                <h4>Jane Doe</h4>
+            </div>
+            <div class="avatar w-half">
+                <img src="images/avatar.jpg" alt="avatar">
+                <h4>Lorem Ipsum</h4>
+            </div>
+            <div class="avatar w-half">
+                <img src="images/dolor_sit.jpg" alt="jon_doe">
+                <h4>Dolor Sit</h4>
+            </div>
         </div>
     </div>
 </div>
+
     
 </div>
 </main>
