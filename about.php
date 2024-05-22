c1e7c7d0e8b37cc7e8ea46fd0c46b71b70395021
LucNogSoares <Lucasnogsoares@gmail.com>
2024-05-22 21:07:12 +0100

    header image

--- about.php
+++ about.php
@@ -7,18 +7,18 @@
     <div>
         <div>
             <h2>Our History</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
+            <p>Grafton Home is a leading property and homes portal. Established in 1999, the company has transformed the experience of looking for a home for hundreds of thousands of house hunters and has also provided estate agents with an efficient and effective marketing tool.</p>
+            <p>The real state has been growing its business since and just right after the Pandemic where people are more into an online search and especially for property with quality and good reputation. </p>
         </div>
         <br>
         <div>
             <h2>Make Home in Ireland for Generations</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias illum est recusandae rem. Provident a sed dignissimos repellat labore cumque. Perspiciatis minus ratione cupiditate expedita? Sed error ducimus eaque fugit!</p>
+            <p>Grafton Home is a sales and marketing company which focuses on delivering results to our customers, clients and people.MyHome also has an extensive Services section which allows users to find the best quote for a job no matter where you are in Ireland. Users can also search a comprehensive directory of Service Providers or just browse our many articles on Home Improvement tips and advice.</p>
+            <p></p>
         </div>
     </div>
     <div>
-        <h2>Our Team</h2>
+        <h2>Meet Our Team</h2>
         <div class="avatar-group">
             <div class="avatar w-half">
                 <img src="images/jhon_doe.jpg" alt="jon_doe">
